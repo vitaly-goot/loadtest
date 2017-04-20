9f9ef5074c6ed7f0b57b4114be7cfa8d9613109e
Vitaly Goot <vitaly.goot@gmail.com>
2017-04-20 16:38:38 -0700

    modify bin/debug to work with couch{1,2}

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,14 +1,79 @@
-#!/bin/bash
+#!/bin/bash 
 
-ME="dtop_"$$"@localhost"
+usage(){
+    cat <<- end
+usage: `basename $0` [-setcookie <cookie>] [-nocookie] [-nettick <tick>] 
+       [-win] [-dbg] [-vsn <OTP version>] [ -targ node] [-proxy proxynode] 
+end
+    exit
+}
+
+#ERL=erl
+START="-s dtop start"
+
+while [ -n "$1" ] 
+  do
+  case $1 in
+      "-help"|"-h"|"-?")
+      usage
+      ;;
+      "-dbg")
+      ;;
+      "-win")
+      WIN="xterm -sb -sl 9999 -bg gold -fg black -e"
+      ;;
+      "-vsn"|"-version")
+      shift
+      VSN="+R "$1
+      ;;
+      "-nettick"|"-tick")
+      shift 
+      nettick="-kernel net_ticktime $1"
+      ;;
+      "-cookie"|"-setcookie")
+      shift
+      cookie="-setcookie $1"
+      ;;
+      "-nocookie")
+      cookie=""
+      ;;
+      "-proxy")
+      shift
+      proxy="$1"
+      ;;
+      "-targ")
+      shift
+      TARG="$1"
+      ;;
+      *)
+      if [ $# -eq 1 ]; then
+          usage
+      fi
+      ;;
+  esac
+  shift
+done
+
+
+ME="debug_"$$"@localhost"
 REGION=`grep REGION /a/etc/akamai.conf | awk '{print $1}' | awk -F= '{print $2}'`
 COOKIE=`grep setcookie /a/edgedata/etc/vm.args  | awk '{print $2}'`
-ERL=/a/erlang_R14/bin/erl
-EARGS=(
+if [ -z "$TARG" ]; then 
+    TARG=`grep name /a/edgedata/etc/vm.args  | awk '{print $2}'`
+fi
+couch_version=`/a/bin/cobra_svcvar -r $REGION nsds::use_couch_version`
+
+START="$START $TARG $proxy"
+
+if [[ $couch_version == "2" ]]; then
+
+  ERL=/a/erlang/bin/erl
+  EBIN=/a/edgedata2/lib/couchdb/erlang/lib/nsds_db-1.0/ebin/
+  EARGS=(
     -name $ME
     -proto_dist inet_nsds
-    -pa /usr/local/akamai/edgedata/lib/nsds_db/ebin
-    -pa /usr/local/akamai/edgedata/lib/eper-0.51/ebin
+    -pa /usr/local/akamai/edgedata2/lib/couchdb/erlang/lib/nsds_db-1.0/ebin
+    -pa /usr/local/akamai/edgedata2/lib/couchdb/erlang/lib/eper-0.98.0/ebin
     -setcookie $COOKIE
     -boot start_sasl
     -sasl errlog_type error
@@ -19,4 +84,25 @@ EARGS=(
     +P 10000
  )
 
-$ERL ${EARGS[@]}
+else
+
+  ERL=/a/erlang_R14/bin/erl
+  EARGS=(
+    -name $ME
+    -proto_dist inet_nsds
+    -pa /usr/local/akamai/edgedata/lib/nsds_db/ebin
+    -pa /usr/local/akamai/edgedata/lib/eper-0.98.0/ebin
+    -setcookie $COOKIE
+    -boot start_sasl
+    -sasl errlog_type error
+    -hidden
+    $nettick
+    +K true
+    +A 32
+    +P 10000
+ )
+
+fi
+
+$ERL ${EARGS[@]} $START
+
--- couch/dirview.py
+++ couch/dirview.py
@@ -1,6 +1,81 @@
 #/a/bin/python2.7
 
 def load_test_do_work(ip, doc, args, **params):
+    if args['dirview'].has_key('debug'): debug = args['dirview']['debug']
+    else: debug = False
+
+    local = getattr(params['local'], 'local')
+    if not local['init']:
+        local['init'] = True
+        local['conn'] = None
+        local['status'] = {}
+        local['counter'] = 0 
+        local['heartbit'] = int(random.random() * args['upload']['heartbit'])
+
+    if local['conn'] == None:
+        local['conn'] = httplib.HTTPConnection(ip, 5050)
+
+    local['counter'] += 1
+
+    options = CouchUtils.parse_args(args['dirview'])
+    key = "%s" %  doc.replace("%2F", "/%2F").split('%2F')
+    startkey = urllib.quote_plus(key.replace('\'', '"').replace(' ', ''))
+    options.append('startkey=%s' % startkey)
+
+    if args.has_key('viewname'): viewname = args['viewname']
+    else: viewname = 'dirview'
+
+    time.sleep(random.random()*4)
+
+    for i in range(params['nrevs']):
+        if debug: print >>sys.stderr, "%d [%s] GET /%s/_design/active/_view/%s?%s" % (time.time(), ip, params['dbname'],viewname,"&".join(options))
+
+        try: 
+            local['conn'].request("GET", "/%s/_design/active/_view/%s?%s" %(params['dbname'],viewname,"&".join(options)))
+            resp = local['conn'].getresponse()
+            if local['status'].has_key(resp.status):
+                local['status'][resp.status] += 1
+            else:   
+                local['status'][resp.status] = 1
+        except httplib.BadStatusLine:
+            print >>sys.stderr, "%d [%s] BadStatusLine in GET /%s/_design/active/_view/%s?%s" % (time.time(), ip, params['dbname'],viewname,"&".join(options))
+            #traceback.print_exc(file=sys.stderr)
+            local['conn'].close()
+            time.sleep(random.random())
+            local['conn'] = None
+            if local['status'].has_key(500):
+                local['status'][500] += 1
+            else:
+                local['status'][500] = 1
+            break
+        except SocketError as e:
+            print >>sys.stderr, "%d [%s] SocketError %s in GET /%s/_design/active/_view/%s?%s" % (time.time(), ip, e, params['dbname'],viewname,"&".join(options))
+            #traceback.print_exc(file=sys.stderr)
+            local['conn'].close()
+            time.sleep(random.random())
+            local['conn'] = None
+            if local['status'].has_key(600):
+                local['status'][600] += 1
+            else:
+                local['status'][600] = 1
+            break
+        pass    
+
+        out = ''
+        chunk = resp.read()
+        while chunk:
+            out += chunk
+            chunk = resp.read()
+
+        if debug: print >>sys.stderr, "%d [%s] %s" % (time.time(), ip, out)
+    pass
+
+    if local['counter'] % args['dirview']['heartbit'] == local['heartbit']:
+        print >>sys.stderr, "%d [%s] HEARTBIT (%d) %d %s" % (time.time(), ip, local['tid'], local['counter'] * params['nrevs'], str(sorted(local['status'].items())).replace(' ', ''))
+        local['status'] = {}
+
+
+def _load_test_do_work(ip, doc, args, **params):
     for i in range(params['nrevs']):
         out, _err = CouchAPI.read_view_front_door(ip, doc, args['dirview'], **params)
 
